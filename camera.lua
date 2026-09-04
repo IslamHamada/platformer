34b5deb1d464e6a094fedae58f02932dfe3f8a90
IslamHamada <Islamhamada222@gmail.com>
2026-09-04 04:42:30 +0200

    add camera shake settings

--- camera.lua
+++ camera.lua
@@ -4,6 +4,12 @@ local camera = {}
 
 function camera.load(player, map)
     camera.y = map.currentLevel.height * map.y_scale - globals.height
+    camera.x = 0
+
+    camera.shake_factor = 0
+    camera.shake = false
+    camera.shake_time = 0
+    camera.shake_timer = 0
 end
 
 function camera.update(dt, player, map)
@@ -17,6 +23,24 @@ function camera.update(dt, player, map)
     end
 
     camera.y = utils.lerp(camera.y, targetY, dt * 5)
+
+
+    ------------------------------------------------------
+    if camera.shake then
+        camera.shake_timer = camera.shake_timer + dt
+        if camera.shake_timer > camera.shake_time then
+            camera.shake_timer = 0
+            camera.shake = false
+        end
+        camera.x = math.random(-camera.shake_factor, camera.shake_factor)
+        camera.y = camera.y + math.random(-camera.shake_factor, camera.shake_factor)
+    end
+end
+
+function camera.init_shake(time, factor)
+    camera.shake_time = time
+    camera.shake_factor = factor
+    camera.shake = true
 end
 
 return camera
